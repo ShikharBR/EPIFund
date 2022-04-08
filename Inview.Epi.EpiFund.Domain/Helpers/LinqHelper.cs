7260985efd697f49494b5746b74bcf12fa0a4081
Shikhar Dubey <shikhar@bosframework.com>
2022-04-08 14:31:23 +0530

    Backend changes

---
+++ Inview.Epi.EpiFund.Domain/Helpers/LinqHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inview.Epi.EpiFund.Domain.Helpers
+{
+    public static class LinqHelper
+    {
+        public static IQueryable<T> AddSearchParameter<T>(this IQueryable<T> query, bool condition, System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            if (condition)
+                query = query.Where(predicate);
+            return query;
+        }
+    }
+}
